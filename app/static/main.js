e98a8f700100f32b9b10fea7838ff61ddd98595b
Cedric Murairi <murairicedric@gmail.com>
2020-06-24 22:14:40 +0200

    adding and removing entries to the db and fontend

--- app/static/main.js
+++ app/static/main.js
@@ -1,8 +1,20 @@
 var visitor_detail_button = document.querySelectorAll('.visitor_detail_button');
-var exit_button_all = document.querySelectorAll('.exit_button_all');
+var exit_button_all = document.querySelectorAll('.exit_button');
 console.log(exit_button_all);
 var search_form_active = document.querySelector('#active_search');
 var search_form_all = document.querySelector('#all_search');
+
+document.querySelectorAll('.visitor_detail_button').forEach(function(button){
+	button.addEventListener('click', function(){
+		var id = button.dataset.id;
+		document.querySelectorAll('.show_details').forEach(function(card_detail){
+			if(card_detail.dataset.id === id){
+				card_detail.style.display = "block";
+				document.querySelector('.cover').style.display = "block";
+			}
+		});
+	})
+});
 
 exit_button_all.forEach(function(button){
 	button.addEventListener('click', function(){
@@ -22,11 +34,11 @@ exit_button_all.forEach(function(button){
 						document.querySelectorAll('.card').forEach(function(card){
 							var id = card.dataset.id;
 							if(id === visitor_id){
-								card.style.display = "none";
+								card.style.animationPlayState = "running";
 							}
 						});
 
-						history.pushState(null, 'exit', 'exit')
+						// history.pushState(null, 'exit', 'exit')
 					}
 					else
 						console.log("nothing here little boy");
